d3c1744bedb1ac8463674e75955c91d1e20d6c5a
iamayushdas <ayush24das@gmail.com>
2021-01-01 21:15:46 +0530

    nav-links added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
-import {FaAlignRight} from 'react-icons/fa';
+import { FaAlignRight } from 'react-icons/fa'; 
 import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
@@ -9,19 +9,34 @@ export default class Navbar extends Component {
     }
     handleToggle = () => {
         this.setState({isOpen:!this.state.isOpen})
-    }
+    };
     render() {
         return (
-            <nav className="navbar">
-                <div className="nav-center">
-                <div className="nav-header">
-                <Link to = "/">
-                <img src={logo} alt="Beach Resort"></img>
+          <nav className="navbar">
+            <div className="nav-center">
+              <div className="nav-header">
+                <Link to="/">
+                  <img src={logo} alt="Beach Resort"></img>
                 </Link>
-                </div>
-                </div>
-                
-            </nav>
-        )
+                <button 
+                    type="button" 
+                    className="nav-btn" 
+                    onClick={this.handleToggle}
+                >
+                    <FaAlignRight className="nav-icon"/>
+                </button>
+              </div>
+              <ul className = {this.state.isOpen?"nav-links show-nav":"nav-links"}>
+                <li>
+                  <Link TO="/">Home</Link>
+                </li>
+                <li>
+                  <Link TO="/rooms">Rooms</Link>
+                </li>    
+               
+              </ul>
+            </div>
+          </nav>
+        );
     }
 }
