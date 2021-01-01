c775baaf490ed35380c4349e11089b4d4a9facd5
iamayushdas <ayush24das@gmail.com>
2021-01-01 21:50:50 +0530

    Banner added to error and home page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Banner from '../components/Banner'
 import Hero from '../components/Hero'
+import {Link} from 'react-router-dom'
 function Error() {
     return (
-        <Hero />
+        <Hero>
+            <Banner title="404" subtitle="page not found">
+            <Link to='/' className = "btn-primary">
+                Return Home
+            </Link>
+            </Banner>
+        </Hero>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import Hero from '../components/Hero';
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 function Home() {
     return (
-        <Hero />
+        <Hero>
+            <Banner title="luxurious table" subtitle="deluxe table starting at 499 INR">
+                <Link to='/rooms' className="btn-primary">
+                    Our tables
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
