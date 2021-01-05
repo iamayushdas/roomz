172ef610a680832d5f81632d8cb7e95a968160b9
iamayushdas <ayush24das@gmail.com>
2021-01-05 22:27:26 +0530

    context API done

--- src/components/FeaturedTables.js
+++ src/components/FeaturedTables.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import { TableContext } from '../context'
 export default class FeaturedTables extends Component {
+    static contextType = TableContext
     render() {
+        const {name, greeting} = this.context;
+       
         return (
             <div>
-                hello from featured rooms
+                {greeting} {name} from featured rooms
             </div>
         )
     }
--- src/context.js
+++ src/context.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 const TableContext = React.createContext();
 export default class TableProvider extends Component {
     state={
-
+        greeting :'hello',
+        name: 'Shivani'
     }
     render() {
         return (
-            <TableContext.Provider value="hello">
+            <TableContext.Provider value={{...this.state}}>
             {this.props.children}    
             </TableContext.Provider >
         )
