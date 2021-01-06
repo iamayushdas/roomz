e5277f1a1777cb023c63b37082a0003c0896fcbb
iamayushdas <ayush24das@gmail.com>
2021-01-06 22:49:16 +0530

    data.js fetched

--- src/context.js
+++ src/context.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react'
+import items from './data'
 const TableContext = React.createContext();
 export default class TableProvider extends Component {
-    state={
-        greeting :'hello',
-        name: 'Shivani'
-    }
-    render() {
-        return (
-            <TableContext.Provider value={{...this.state}}>
-            {this.props.children}    
-            </TableContext.Provider >
-        )
-    }
+  state = {
+    tables: [],
+    sortedRooms: [],
+    featuredTables: [],
+    loading: true,
+  };
+
+  // getData
+
+  componentDidMount() {
+    let tables = this.formatData(items);
+    console.log(tables);
+  }
+
+  formatData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => image.fields.file.url);
+
+      let table = { ...item.fields, images, id };
+      return table;
+    });
+    return tempItems;
+  }
+
+  render() {
+    return (
+      <TableContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </TableContext.Provider>
+    );
+  }
 }
 
 const TableConsumer = TableContext.Consumer;
